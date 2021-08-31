825175a64795c96a0a0976368f910adca7b5d880
kietlt <kiet.kyle.le1993@gmail.com>
2021-08-31 17:41:39 +0700

    Update message Object name unity sender

--- SwiftPlugin/Source/BleUnitySender.m
+++ SwiftPlugin/Source/BleUnitySender.m
@@ -7,43 +7,41 @@
 
 #import "BleUnitySender.h"
 
-extern void UnitySendMessage(const char *, const char *, const char *);
-
 @implementation BleUnitySender
 static NSString* recieverObjectName = @"BleReceiver";
 
 + (void)didBleManagerChangeStateWith:(NSString *)state
 {
-//    UnitySendMessage([recieverObjectName UTF8String], [@"didBleManagerChangeStateWith" UTF8String], [state UTF8String]);
+//    UnitySendMessage([recieverObjectName UTF8String], [@"DidBleManagerChangeStateWith" UTF8String], [state UTF8String]);
 }
 
 + (void)didScanTimeOut
 {
-//    UnitySendMessage([recieverObjectName UTF8String], [@"didScanTimeOut" UTF8String], nil);
+//    UnitySendMessage([recieverObjectName UTF8String], [@"DidScanTimeOut" UTF8String], nil);
 }
 
 + (void)didConnectPeripheralWith:(NSString *)state
 {
-//    UnitySendMessage([recieverObjectName UTF8String], [@"didConnectPeripheralWith" UTF8String], [state UTF8String]);
+//    UnitySendMessage([recieverObjectName UTF8String], [@"DidConnectPeripheralWith" UTF8String], [state UTF8String]);
 }
 
 + (void)didDiscoverZealLe0:(NSString *)device
 {
-//    UnitySendMessage([recieverObjectName UTF8String], [@"didDiscoverZealLe0" UTF8String], [device UTF8String]);
+//    UnitySendMessage([recieverObjectName UTF8String], [@"DidDiscoverZealLe0" UTF8String], [device UTF8String]);
 }
 
 + (void)didReceiveBloodPressureData:(NSString *)data
 {
-//    UnitySendMessage([recieverObjectName UTF8String], [@"didReceiveBloodPressureData" UTF8String], [data UTF8String]);
+//    UnitySendMessage([recieverObjectName UTF8String], [@"DidReceiveBloodPressureData" UTF8String], [data UTF8String]);
 }
 
 + (void)deviceDidChangeStatePowerOff
 {
-//    UnitySendMessage([recieverObjectName UTF8String], [@"deviceDidChangeStatePowerOff" UTF8String], nil);
+//    UnitySendMessage([recieverObjectName UTF8String], [@"DeviceDidChangeStatePowerOff" UTF8String], nil);
 }
 
 + (void)didUpdateMeasureStep:(NSString *)stepsString
 {
-//    UnitySendMessage([recieverObjectName UTF8String], [@"didUpdateMeasureStep" UTF8String], [stepsString UTF8String]);
+//    UnitySendMessage([recieverObjectName UTF8String], [@"DidUpdateMeasureStep" UTF8String], [stepsString UTF8String]);
 }
 @end
